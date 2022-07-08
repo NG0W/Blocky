d36051678d8318b38007f5cd8057735b97a819ef
Sid-Ahmed_MIR_Unblocked <smir@unblocked-group.com>
2022-07-08 05:43:40 +0200

    wallet connection, getting storage and mint button

---
+++ utils/config.js
@@ -0,0 +1,7 @@
+const config = {
+  NFTcontractAddress: "KT19mTC36Mt27bP2UbekgF4seffMoWAQxgdB",
+  rpc: "https://jakartanet.tezos.marigold.dev/",
+  network: "jakartanet",
+  logo: "https://img.lovepik.com/free-png/20220125/lovepik-real-estate-building-logo-png-image_401737177_wh860.png",
+};
+export default config;

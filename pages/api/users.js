29b374c910d72b86e3ae7ea9a4fd53f9feaffe94
Gautier <gautier.roux@finelia.fr>
2022-07-10 13:32:49 +0200

    Added users.js api

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,6 +1,5 @@
 import sql from '../../database/db'
 
-export default async function getUsers(req, res) {
-    const users = await sql`select * from users`;
-    res.status(200).json({ users: users });
+export default async function getUsers() {
+    return await sql`select * from users`;
 }
